52dfae216d3d5736f9baa0b8329871bf45f143e0
Miguel del Río Martínez <mdelrmart@gmail.com>
2025-01-28 12:56:11 +0100

    adat ud4

---
+++ ADAT/UD4/EmpresaHBU4A1/src/empresahbu4a1/Operaciones.java
@@ -0,0 +1,156 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empresahbu4a1;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import pojos.Departamento;
+import pojos.Empregado;
+
+/**
+ *
+ * @author usuario
+ */
+public class Operaciones {
+    
+    public static Session iniciarSesion() {
+        Session sesion = HibernateUtil.getSessionFactory().openSession();
+        return sesion;
+    }
+
+    public static void cerrarSesion(Session sesion) {
+        if (sesion != null) {
+            sesion.close();
+        }
+    }
+
+    public static <T> void insertarObjeto(T objeto) {
+        Session sesion = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+
+        try {
+            tx = sesion.beginTransaction();
+            sesion.save(objeto);
+            tx.commit();
+        } catch (HibernateException e) {
+            System.out.println("No ha funcionado " + e.getMessage());
+            if (tx != null) {
+                tx.rollback();
+            }
+        }
+    }
+    
+    public static void getDepartamento(int numDepartamento){
+        try {
+            Session session = iniciarSesion();
+            Departamento departamento = (Departamento) session.load(Departamento.class, numDepartamento);
+            System.out.println(departamento.getNumDepartamento());
+            System.out.println(departamento.getNomeDepartamento());
+        } catch (Exception e) {
+            System.out.println("No se ha encontrado el departamento " + numDepartamento);
+        }
+    
+    }
+    
+    public static void getEmpleado(String nss){
+        try {
+            Session session = iniciarSesion();
+            Empregado empregado = (Empregado) session.load(Empregado.class, nss);
+            System.out.println(empregado.getNss());
+            System.out.println(empregado.getNome() + " " + empregado.getApelido1() + " " + empregado.getApelido2());
+        } catch (Exception e) {
+            System.out.println("No se ha encontrado el empleado " + nss);
+        }
+    
+    }
+    
+    public static void borrarDepartamento(int numDepartamento, Session sesion){
+        Transaction tx = null;
+
+        Departamento dep = new Departamento(numDepartamento, "");
+        
+        try {
+            tx = sesion.beginTransaction();
+            sesion.delete(dep);
+            tx.commit();
+        } catch (HibernateException e) {
+            System.out.println("No ha funcionado " + e.getMessage());
+            if (tx != null) {
+                tx.rollback();
+            }
+        }
+    }
+    
+    public static void cambiarNombreDepartamento(int numDepartamento, String nuevoNombre, Session sesion) {
+        Transaction tx = null;
+
+        try {
+            tx = sesion.beginTransaction();
+            Departamento dep = (Departamento) sesion.get(Departamento.class, numDepartamento);
+            dep.setNomeDepartamento(nuevoNombre);
+            //sesion.update(dep);
+            tx.commit();
+        } catch (HibernateException e) {
+            System.out.println("No ha funcionado " + e.getMessage());
+            if (tx != null) {
+                tx.rollback();
+            }
+        }
+    }
+        
+    //--------------------------------------------------------------------------
+    
+    public static void insertarEmpleado(Empregado emp, Session sesion) {
+        Transaction tx = null;
+
+        try {
+            tx = sesion.beginTransaction();
+            sesion.save(emp);
+            tx.commit();
+        } catch (HibernateException e) {
+            System.out.println("No ha funcionado " + e.getMessage());
+            if (tx != null) {
+                tx.rollback();
+            }
+        }
+    }
+    
+    public static void insertarDepartamento(Departamento dep, Session sesion) {
+        Transaction tx = null;
+
+        try {
+            tx = sesion.beginTransaction();
+            sesion.save(dep);
+            tx.commit();
+        } catch (HibernateException e) {
+            System.out.println("No ha funcionado " + e.getMessage());
+            if (tx != null) {
+                tx.rollback();
+            }
+        }
+    }
+    
+        public static void insertarDepartamentoNombre(String nombre, Session sesion) {
+        Transaction tx = null;
+
+        Departamento dep = new Departamento(0, nombre);
+        
+        try {
+            tx = sesion.beginTransaction();
+            sesion.save(dep);
+            tx.commit();
+        } catch (HibernateException e) {
+            System.out.println("No ha funcionado " + e.getMessage());
+            if (tx != null) {
+                tx.rollback();
+            }
+        }
+    }
+    
+    //--------------------------------------------------------------------------
+    
+}
