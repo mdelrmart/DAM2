71a494012e7ef3c8fac121f728c165a03bc44e03
Miguel del Río Martínez <mdelrmart@gmail.com>
2025-02-20 12:45:46 +0100

    Update ConsultasHB.java

--- ADAT/UD4/EMPRESAHB2_ALU25XML/src/empresahb2_feb25/ConsultasHB.java
+++ ADAT/UD4/EMPRESAHB2_ALU25XML/src/empresahb2_feb25/ConsultasHB.java
@@ -55,7 +55,7 @@ public class ConsultasHB {
 
     public static void mostrarEmpregadosTipo(String tipo) {
         sesion.beginTransaction();
-        
+
         List<Object[]> results = sesion.createQuery("""
                 select e.nss, concat(e.apelido1, ' ', isnull (e.apelido2,''), ', ', e.nome) as nome_completo, e.departamento.nomeDepartamento,
                        case type(e)
