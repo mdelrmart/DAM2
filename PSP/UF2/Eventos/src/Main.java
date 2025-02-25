136cf4c11f6e189320569ccc28b44f0c947d9383
mdelrmart <mdelrmart@gmail.com>
2025-02-26 00:52:00 +0100

    pser

--- PSP/UF2/Eventos/src/Main.java
+++ PSP/UF2/Eventos/src/Main.java
@@ -1,13 +1,11 @@
-import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int option = 0;
+        int opcion = 0;
 
-        while (option != 5) {
-            System.out.println("\n");
+        while (opcion != 5) {
             System.out.println("Menu:");
             System.out.println("1. Introducir evento");
             System.out.println("2. Consultar disponibilidad");
@@ -16,14 +14,14 @@ public class Main {
             System.out.println("5. Salir");
             System.out.print("Seleccione una opción: ");
 
-            option = scanner.nextInt();
+            opcion = scanner.nextInt();
 
-            switch (option) {
+            switch (opcion) {
                 case 1:
                     introducirEvento();
                     break;
                 case 2:
-                    consultarDisponibilidad();
+                    insertarAsistenteEvento();
                     break;
                 case 3:
                     eliminarEvento();
@@ -42,6 +40,7 @@ public class Main {
         scanner.close();
     }
 
+    // 1
     public static void introducirEvento() {
         while (true) {
             Scanner scanner = new Scanner(System.in);
@@ -49,7 +48,7 @@ public class Main {
             System.out.print("Introduzca el nombre del evento: ");
             String nombre = scanner.nextLine();
 
-            // If the user enters an empty string, we break the loop
+            // Si el nombre está vacío, salimos del bucle
             if (nombre.equals("")) {
                 break;
             }
@@ -61,6 +60,47 @@ public class Main {
         }
     }
 
+    // 2
+    public static void insertarAsistenteEvento() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduzca el número de personas que asistirán al evento: ");
+        int asistentes = scanner.nextInt();
+
+        //Operaciones.consultarDisponibilidad(asistentes);
+
+        // Si no hay disponibilidad, salimos
+        if (!Operaciones.consultarDisponibilidad(asistentes)) {
+            return;
+        }
+
+        System.out.println("Introduzca el código del evento al que desea agregar asistentes: ");
+        int codEvento = scanner.nextInt();
+
+        int contadorAsistentes = 0;
+
+        scanner.nextLine();
+
+        while (true) {
+            if (contadorAsistentes >= asistentes) {
+                System.out.println("Se han agregado todos los asistentes.");
+                break;
+            }
+
+            System.out.println("Introduzca el nombre del asistente: ");
+            String nombre = scanner.nextLine();
+
+            if (nombre.equals("")) {
+                break;
+            }
+
+            Operaciones.insertarAsistenteEvento(codEvento, nombre);
+            contadorAsistentes++;
+        }
+
+    }
+
+    // 3
     public static void eliminarEvento() {
         Scanner scanner = new Scanner(System.in);
 
@@ -70,17 +110,9 @@ public class Main {
         Operaciones.eliminarEvento(nombre);
     }
 
+    // 4
     public static void visualizarDatos() {
         Operaciones.visualizarDatos();
     }
 
-    public static void consultarDisponibilidad() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Introduzca el número de personas que asistirán al evento: ");
-        int sitios = scanner.nextInt();
-
-        Operaciones.consultarDisponibilidad(sitios);
-
-    }
 }
--- PSP/UF2/Eventos/src/Operaciones.java
+++ PSP/UF2/Eventos/src/Operaciones.java
@@ -4,6 +4,8 @@ import java.net.*;
 import org.json.*;
 
 public class Operaciones {
+
+    // region Opcion 1
     public static void insertarEvento(String nombre, int maxAsistentes) {
         URL url = null;
         HttpURLConnection con = null;
@@ -25,7 +27,7 @@ public class Operaciones {
             out.close();
 
             con.connect();
-            if (con.getResponseCode() == 201) {
+            if (con.getResponseCode() == 200) {
                 /* Si en la inserción devolvemos un JSON con la clave generada, aquí deberíamos
                 recuperar el JSON y analizarlo para obtenerla por si la necesitamos */
 
@@ -37,8 +39,10 @@ public class Operaciones {
             System.out.println("Error en la conexión");
         }
     }
+    // endregion
 
-    public static void consultarDisponibilidad(int maxAsistentes) {
+    // region Opcion 2
+    public static boolean consultarDisponibilidad(int maxAsistentes) {
         URL url = null;
         HttpURLConnection con = null;
         String json = "";
@@ -63,24 +67,66 @@ public class Operaciones {
 
                 bufferIn.close();
 
-                /* Analizamos el JSON devuelto, que sabemos que es un array de objetos cliente */
+                /* Analizamos el JSON devuelto, que sabemos que es un array de objetos */
                 JSONArray datos = new JSONArray(json);
                 for (int i = 0; i < datos.length(); i++) {
-                    JSONObject cliente = datos.getJSONObject(i);
+                    JSONObject evento = datos.getJSONObject(i);
 
-                    String nombre = cliente.getString("nombre");
-                    int sitiosDisponibles = cliente.getInt("sitiosDisponibles");
+                    int codEvento = evento.getInt("codEvento");
+                    String nombre = evento.getString("nombre");
+                    int sitiosDisponibles = evento.getInt("sitiosDisponibles");
 
-                    System.out.printf("Evento: %s - Sitios disponibles: %d\n", nombre, sitiosDisponibles);
+                    System.out.printf("Código: %d - Evento: %s - Sitios disponibles: %d\n", codEvento, nombre, sitiosDisponibles);
                 }
+                return true;
+            } else if (con.getResponseCode() == 404) {
+                System.out.println("No hay eventos disponibles para el número de asistentes introducido");
+                return false;
             } else {
                 System.out.println("Problemas. Respuesta: (" + con.getResponseCode() + ") " + con.getResponseMessage());
             }
         } catch (IOException ex) {
             System.out.println("Error en la conexión");
         }
+        return false;
     }
 
+    public static void insertarAsistenteEvento(int codEvento, String nombre) {
+        URL url = null;
+        HttpURLConnection con = null;
+        String json = "";
+        String strURL = "http://localhost/eventos/rest.php/insertarAsistenteEvento";
+
+        try {
+            String parametros =
+                    "codEvento=" + URLEncoder.encode(String.valueOf(codEvento), "UTF-8") + "&nombre=" + URLEncoder.encode(nombre, "UTF-8");
+
+            url = new URL(strURL);
+            con = (HttpURLConnection) url.openConnection();
+
+            // le pasamos los parámetros en el cuerpo de la petición
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            PrintWriter out = new PrintWriter(con.getOutputStream());
+            out.print(parametros);
+            out.close();
+
+            con.connect();
+            if (con.getResponseCode() == 200) {
+                /* Si en la inserción devolvemos un JSON con la clave generada, aquí deberíamos
+                recuperar el JSON y analizarlo para obtenerla por si la necesitamos */
+
+                System.out.println("Se ha insertado el asistente correctamente");
+            } else {
+                System.out.println("Problemas al insertar el asistente en el evento. Respuesta: (" + con.getResponseCode() + ") " + con.getResponseMessage());
+            }
+        } catch (IOException ex) {
+            System.out.println("Error en la conexión");
+        }
+    }
+    // endregion
+
+    // region Opcion 3
     public static void eliminarEvento(String nombre) {
         URL url = null;
         HttpURLConnection con = null;
@@ -114,12 +160,16 @@ public class Operaciones {
             System.out.println("Error en la conexión");
         }
     }
+    // endregion
 
+    //region Opcion 4
     public static void visualizarDatos() {
         URL url = null;
         HttpURLConnection con = null;
         String json = "";
         String strURL = "http://localhost/eventos/rest.php/eventos";
+
+        System.out.print("\n");
 
         try {
             url = new URL(strURL);
@@ -187,17 +237,18 @@ public class Operaciones {
 
                     System.out.printf("- %s\n", nombre);
                 }
-                System.out.println("\n");
+                System.out.print("\n");
             } else if (con.getResponseCode() == 404) {
                 System.out.println("No hay asistentes para este evento");
-                System.out.println("\n");
+                System.out.print("\n");
             } else {
                 System.out.println("Problemas. Respuesta: (" + con.getResponseCode() + ") " + con.getResponseMessage());
-                System.out.println("\n");
+                System.out.print("\n");
             }
         } catch (IOException ex) {
             System.out.println("Error en la conexión");
         }
     }
+    //endregion
 
 }
